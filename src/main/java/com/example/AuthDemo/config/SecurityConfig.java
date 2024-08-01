08e88200928ec8f391a337f0c7acf476da4aa222
biplov31 <biplovthapa31@gmail.com>
2024-08-01 18:23:33 +0545

    JWT implementation

--- src/main/java/com/example/AuthDemo/config/SecurityConfig.java
+++ src/main/java/com/example/AuthDemo/config/SecurityConfig.java
@@ -1,16 +1,9 @@
 package com.example.AuthDemo.config;
 
-import com.example.AuthDemo.repository.KiranaUserRepository;
 import com.example.AuthDemo.service.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
